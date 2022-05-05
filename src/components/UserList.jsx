4ffb000ab8534dd8293a443e9731b5971bc8f7c9
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-05-05 16:35:34 +0300

    return UserItem inside UserList

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserItem from './UserItem';
 
 function UserList(props){
     const {users} = props;
@@ -6,15 +7,11 @@ function UserList(props){
         <div>
             {
                 users.map((user) => {
-                    return <div>
-                        <h2>{user.name}</h2>
-                        <p>{user.email}</p>
-                        {
-                            user.isGoldClient
-                                ? <p>CLIENT GOLD</p>
-                                : null 
-                        }
-                    </div>
+                    return <UserItem 
+                        name = {user.name}
+                        email = {user.email}
+                        isGoldClient = {user.isGoldClient}
+                    />
                 }) 
             }   
         </div>
