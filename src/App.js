13be9db42c5bf5936719241b72d264c49d6f8cc3
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-05-05 15:52:44 +0300

    save all users in state and display users
    desi scopul final este să luăm userii cu datele lor dintr-un formular
    sau chiar dintr-un api, momentan am salvat toate datele userilor in state
    pentru a înțelege mai clar cum se fac tranzițiile intre state și
    valorile trimise către user.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,19 @@ class App extends React.Component{
   constructor(){
     super();
     this.state = {
-      background: 'white'
+      background: 'white',
+      users: [
+        {
+          name: "Arsene Florin",
+          email: "arsene.florin@gmail.com",
+          isGoldClient: true
+        },
+        {
+          name: "Criste Mihai",
+          email: "criste.mihai@gmail.com",
+          isGoldClient: false
+        }
+      ]
     }
   }
 
@@ -22,8 +34,16 @@ class App extends React.Component{
     return (
       <div className="App" style={{background: this.state.background}}>
         <h1>Lista Utilizatori:</h1>
-        <UserItem name="Arsene Florin" email="arsene.florin@gmail.com" isGoldClient={true}/>
-        <UserItem name="Criste Mihai" email="criste.mihai@gmail.com" isGoldClient={false}/>
+        <UserItem 
+          name={this.state.users[0].name} 
+          email={this.state.users[0].email} 
+          isGoldClient={this.state.users[0].isGoldClient} 
+        />
+        <UserItem 
+          name={this.state.users[1].name} 
+          email={this.state.users[1].email} 
+          isGoldClient={this.state.users[1].isGoldClient} 
+        />
           <input type="color" onChange={ (event) => this.handleBackgroundChange(event) }/>
       </div>
     );
